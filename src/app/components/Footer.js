8f400c5770a9c89f7b776300ba133f4004da17e0
temevh <temehama@gmail.com>
2025-01-04 13:22:58 +0200

    updated footer

--- src/app/components/Footer.js
+++ src/app/components/Footer.js
@@ -3,7 +3,7 @@ import FooterSocials from "./FooterSocials";
 
 const Footer = () => {
   return (
-    <div className="pt-4 flex items-center justify-between w-full">
+    <div className="pt-4 flex items-center justify-between w-full pb-4">
       <p className="text-sm text-graytext mt-4">Teemu Hämäläinen</p>
       <span>
         <FooterSocials />
