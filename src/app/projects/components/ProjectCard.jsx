3ec5747261b2774c69c7def64de3de5b1a0dd9c3
temevh <temehama@gmail.com>
2025-01-04 10:31:36 +0200

    projects displayed

--- src/app/projects/components/ProjectCard.jsx
+++ src/app/projects/components/ProjectCard.jsx
@@ -1,3 +1,21 @@
-const ProjectCard = () => {};
+const ProjectCard = ({ project }) => {
+  return (
+    <div className="flex flex-row">
+      <img
+        src={project.img}
+        alt={project.title}
+        className="w-40 h-40 rounded-[10px] object-cover border-[1px] border-secondary"
+      />
+      <div className="pl-4">
+        <p className="text-graytext text-md">{project.desc}</p>
+        <div className="flex flex-row gap-2">
+          {project.tags.map((tag) => (
+            <p className="text-sm bg-gray-500 px-1 rounded-md">{tag}</p>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default ProjectCard;
--- src/app/projects/page.jsx
+++ src/app/projects/page.jsx
@@ -10,13 +10,19 @@ const ProjectsPage = () => {
       code: "https://github.com/temevh/tuuraaja",
     },
     {
-      title: "Arduino alarm system",
-      desc: "A fairly complex alarm system made with two Arduino boards which are connected with USART. The movement sensor waits for movements and triggers an alarm protocol when triggered",
-      tags: ["C", "Arduino", "Atmel studio"],
-      img: "https://user-images.githubusercontent.com/96690178/259686989-b7ab8e2d-f22a-4dca-803c-aca4170e6632.jpg",
-      code: "https://github.com/temevh/Arduino-AlarmSystem/tree/main",
+      title: "Maltas data dashboard",
+      desc: "A full-stack dashboard application for healthcare professionals. The dashboard shows data related to the hand washing in a selected unit (hospital, operating room etc.)",
+      tags: ["React", "Typescript", "Node.js", "Next.js"],
+      img: "https://monday.com/blog/wp-content/uploads/2022/03/Untitled-188.png",
+      code: "https://github.com/PROJ-A2023-G08/maltas-data-dashboard",
     },
-
+    {
+      title: "Valuemap",
+      desc: "Valuemap matches job seekers with companies based on shared values, believes and needs. Made for Junction hackathon 2024 ",
+      tags: ["React", "Next.js", "MongoDB", "TailwindCSS"],
+      img: "https://i.ibb.co/XWrHknw/vm3.png",
+      code: "https://github.com/temevh/valuework",
+    },
     {
       title: "Outhere",
       desc: "A mobile app made with Dart and Flutter which gives the user a task to complete every day. Goal is for the user to improve in one category every day (for example sociability, learning or concentration). Very much still a work in progress!",
@@ -24,6 +30,14 @@ const ProjectsPage = () => {
       img: "https://user-images.githubusercontent.com/96690178/264060743-53ccea43-4be3-426c-9f78-1f6a67245932.jpg",
       code: "https://github.com/temevh/outhere",
     },
+    {
+      title: "Arduino alarm system",
+      desc: "A fairly complex alarm system made with two Arduino boards which are connected with USART. The movement sensor waits for movements and triggers an alarm protocol when triggered",
+      tags: ["C", "Arduino", "Atmel studio"],
+      img: "https://user-images.githubusercontent.com/96690178/259686989-b7ab8e2d-f22a-4dca-803c-aca4170e6632.jpg",
+      code: "https://github.com/temevh/Arduino-AlarmSystem/tree/main",
+    },
+
     {
       title: "React review app",
       desc: "An app that lets the user add, review, search and edit books. The app is build around a carousel type of menu where the books are displayed. The app utilizes local storage to save the books and their reviews.",
@@ -45,20 +59,6 @@ const ProjectsPage = () => {
       tags: ["Godot", "GAP", "Game"],
       img: "https://ggj.s3.amazonaws.com/styles/game_content__wide/games/screenshots/2022/01/138869/nayttokuva_154.png?itok=JMBNCLa2&timestamp=1643543481",
       code: "https://github.com/temevh/fgj2022",
-    },
-    {
-      title: "Maltas data dashboard",
-      desc: "A full-stack dashboard application for healthcare professionals. The dashboard shows data related to the hand washing in a selected unit (hospital, operating room etc.)",
-      tags: ["React", "Typescript", "Node.js", "Next.js"],
-      img: "https://monday.com/blog/wp-content/uploads/2022/03/Untitled-188.png",
-      code: "https://github.com/PROJ-A2023-G08/maltas-data-dashboard",
-    },
-    {
-      title: "Valuemap",
-      desc: "Valuemap matches job seekers with companies based on shared values, believes and needs. Made for Junction hackathon 2024 ",
-      tags: ["React", "Next.js", "MongoDB", "TailwindCSS"],
-      img: "https://i.ibb.co/XWrHknw/vm3.png",
-      code: "https://github.com/temevh/valuework",
     },
   ];
 
