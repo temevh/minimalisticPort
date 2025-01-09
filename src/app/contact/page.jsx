09d92cb5796bf4ed30ad4c6a8b28ed9eeac594e9
temevh <temehama@gmail.com>
2025-01-09 09:46:26 +0200

    started to wrk on contact page

---
+++ src/app/contact/page.jsx
@@ -0,0 +1,17 @@
+const ContactPage = () => {
+  return (
+    <div>
+      <p className="text-3xl font-bold">Get in touch✉</p>
+      <div className="flex flex-col gap-4">
+        <p className="text-xl text-white">
+          <span className=" text-graytext">Email:</span>temehama@gmail.com
+        </p>
+        <p className="text-xl text-white">
+          <span className=" text-graytext">Phone number:</span>044-3459658
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default ContactPage;
